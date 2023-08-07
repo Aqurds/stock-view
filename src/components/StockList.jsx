c94ed0049040c3dc2f6741694f5f7778458a39d2
aqurds <omarf1320@gmail.com>
2023-08-08 02:35:29 +0600

    Add search feature

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -1,32 +1,59 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { fetchStockSymbols } from "../store/stock/stockSlice";
 import SingleStock from "./SingleStock";
 import '../styles/stock-list.css';
 
 const StockList = () => {
-  const stockSymbols = useSelector(state => state.stocks.stocks.slice(11500,12500));
+  const stockSymbols = useSelector(state => state.stocks.stocks.slice(11500,11600));
   const stockLoading = useSelector(state => state.stocks.loading);
-  console.log(stockSymbols);
   const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredStockSymbols, setFilteredStockSymbols] = useState([])
+
   useEffect(() => {
     dispatch(fetchStockSymbols())
-  }, [dispatch])
+  }, [])
+
+  useEffect(() => {
+    const newStockSymbols = stockSymbols.filter((stock) => {
+      return stock.includes(searchQuery.trim().toUpperCase())
+    })
+    setFilteredStockSymbols(newStockSymbols)
+  }, [searchQuery])
+
   return (
-    <div className="stock-wrapper">
-      { stockLoading && 
-      <p>
-        Stock loading ...
-      </p> 
-      }
-      { stockSymbols && stockSymbols.map((stock) => {
-        return (
-          <a className="single-stock" href={`/single-stock/${stock}`} key={stock}>
-           {stock}
-          </a>
-        )
-      })}
-    </div>
+    <>
+      <input 
+        type='text' 
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <div className="stock-wrapper">
+        { stockLoading && 
+        <p>
+          Stock loading ...
+        </p> 
+        }
+        { searchQuery ? (
+           filteredStockSymbols && filteredStockSymbols.map((stock) => {
+          return (
+            <a className="single-stock" href={`/single-stock/${stock}`} key={stock}>
+            {stock}
+            </a>
+          )
+        })
+        ) : (
+          stockSymbols && stockSymbols.map((stock) => {
+          return (
+            <a className="single-stock" href={`/single-stock/${stock}`} key={stock}>
+            {stock}
+            </a>
+          )
+        })
+        )}
+      </div>
+    </>
   )
 };
 
