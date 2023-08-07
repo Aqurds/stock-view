09380e6727b2c224c694ffa1a5bea6feed4b96c5
aqurds <omarf1320@gmail.com>
2023-08-07 21:18:00 +0600

    Add redux, api, basic styles in StockList compononent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 import { Routes, Route } from 'react-router-dom';
 import StockList from './components/StockList';
 import SingleStock from './components/SingleStock';
