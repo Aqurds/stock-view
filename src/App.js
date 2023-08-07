76f65364357498074e35f51b30d5cc5674dc4c22
aqurds <omarf1320@gmail.com>
2023-08-07 20:06:18 +0600

    Add React, Set component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import StockList from './components/StockList';
+import SingleStock from './components/SingleStock';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" exact Component={StockList} />
+      <Route path="/single-stock" Component={SingleStock} />
+    </Routes>
+  );
+}
+
+export default App;
