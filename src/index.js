1e0c6a5c48e9af9c45034f1b3d6b1a6378437429
aqurds <omarf1320@gmail.com>
2023-08-10 16:11:26 +0600

    Update Router

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store/globalStore.jsx';
+import StockList from './components/StockList';
+import SingleStock from './components/SingleStock';
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <StockList />,
+    },
+    {
+      path: '/singlestock/:id',
+      element: <SingleStock />,
+    }
+  ]
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
